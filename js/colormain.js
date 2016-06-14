028a2210810eaf2496b07adf3b4bca17971c36f7
Adam Jones <adam@Adams-MacBook-Air.local>
2016-06-14 08:09:59 -0400

    Color Clock just the clock

---
+++ js/colormain.js
@@ -0,0 +1,30 @@
+function startTime() {
+	var today = new Date();
+	var h = today.getHours();
+	var m = today.getMinutes();
+	var s = today.getSeconds();
+	m = checkTime(m);
+	s = checkTime(s);
+	var colorCode = "#" + h + m + s;
+	document.getElementById('txt').innerHTML = colorCode;
+	document.body.style.backgroundColor = colorCode;
+}
+
+function checkTime(i) {
+	if (i<10) {i = "0" + i};
+	return i;
+}
+var t = setInterval(startTime, 500);
+
+
+
+
+
+
+
+
+
+/*function hawaiiTime () {
+	var today = new Date();
+	var h = 
+}*/
